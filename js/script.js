dcd0d9b5355eddccc4cf39a9af215308eb1139aa
samuel <damosisamuel@gmail.com>
2021-01-09 14:10:10 +0100

    molecule btn functionality applied

--- js/script.js
+++ js/script.js
@@ -5,21 +5,47 @@ $(document).ready(function(){
     $('.prev').click(pulsantePrev);
     // a click function for the previous button
 
-  $('.nav i').click(function(){
-    var circlButton =  $('.nav i.active');
-circlButton.removeClass('active');
-    $(this).addClass('active');
+  $('.nav i').click(pulsanteButtonnext);
+
+// $('.nav i').click(pulsanteButtonprev);
+
+// Prova a capire che numero di pallino è (ad esempio il 2) e quindi poi ad aprire l'immagine giusta
 
 
-  });
+
 
 });
 
+
+
+// function for little molecules buttons
+function pulsanteButtonnext(){
+ var elementbtn = $('.images img.active');
+console.log(elementbtn);
+
+var specialBtn1 = $('.nav i.active');
+
+specialBtn1.removeClass('active');
+elementbtn.removeClass('active');
+
+elementbtn.next().addClass('active');
+specialBtn1.next().addClass('active');
+
+if (elementbtn.hasClass('last')){
+  $('.images img.first').addClass('active');
+  $('.nav i.first').addClass('active');
+}
+
+
+}
+// function for little molecules buttons
+
+
+
+// function for next button
 function pulsanteNext(){
 
-  // in questo modo non capisce più quale immagine è attiva
-  // $('.images img.active').removeClass('active');
-  // $('.images img.active').next().addClass('active');
+
 
   var imgAttiva = $('.images img.active');
   console.log(imgAttiva);
